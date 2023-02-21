6222878495e4e04699c188651789b2c7dcc9556f
Zeeshan Malik <zeeshanm2014@gmail.com>
2023-02-20 19:16:40 -0800

    update the server

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:8070', {
+    const response = await fetch('https://codex-9fst.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
